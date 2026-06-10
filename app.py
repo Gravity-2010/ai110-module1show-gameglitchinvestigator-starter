ca6be2551e41828689aa657656399d081ee008eb
Garvita Jain <gjain2@umbc.edu>
2026-06-10 16:00:09 -0400

    fixed the 1st glitch

--- app.py
+++ app.py
@@ -93,6 +93,7 @@ if "secret" not in st.session_state:
     st.session_state.secret = random.randint(low, high)
 
 if "attempts" not in st.session_state:
+    # The glitch was here: st.session_state.attempts = 1.
     st.session_state.attempts = 0
 
 if "score" not in st.session_state:
